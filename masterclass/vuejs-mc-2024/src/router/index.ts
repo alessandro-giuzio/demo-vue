53e1090b034c9aac7cce824121710b96f9d6bd20
alessandro-giuzio <giuzio@icloud.com>
2024-05-31 09:25:18 +0200

    useRoute

--- masterclass/vuejs-mc-2024/src/router/index.ts
+++ masterclass/vuejs-mc-2024/src/router/index.ts
@@ -1,9 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      // Dynamically import the ProjectsView component only when needed
+      component: () => import('@/views/ProjectsView.vue')
+    },
+    {
+      // Define a dynamic route to handle individual project details by their ID
+      path: '/projects/:id',
+      name: 'single-project',
+      component: () => import('@/views/SingleProjectView.vue')
+    },
+  ]
 })
 
 export default router
---
+++ masterclass/vuejs-mc-2024/src/views/SingleProjectView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Project {{ route.params?.id }}</h1>
+    <RouterLink to="/projects">Go to Projects</RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+// Import and initialize useRoute to access route parameters and metadata
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+</script>
