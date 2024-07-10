b73ac6218155e24076329655b931419f734b8a09
alessandro-giuzio <giuzio@icloud.com>
2024-07-10 07:39:43 +0200

    v-if

--- vue-traversy/vue-project/src/App.vue
+++ vue-traversy/vue-project/src/App.vue
@@ -1,3 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'Alessandro',
+      status: 'active',
+    };
+  },
+};
+</script>
+
 <template>
-  <h1>Prova</h1>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
 </template>
