ed91ce43ded0ad253cb35af7ff28741ad2a0d5ee
alessandro-giuzio <giuzio@icloud.com>
2024-08-19 16:16:34 +0200

    cleaning time

--- masterclass/vuejs-mc-2024/src/pages/tasks/index.vue
+++ masterclass/vuejs-mc-2024/src/pages/tasks/index.vue
@@ -3,80 +3,19 @@
 </template>
 
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
-import type { Tables } from '../../../database/types'
-import type { ColumnDef } from '@tanstack/vue-table'
-import { RouterLink } from 'vue-router'
+import { tasksWithProjectsQuery } from '@/utils/supaQueries'
+import type { TasksWithProjects } from '@/utils/supaQueries'
+import { columns } from '@/utils/tableColumns/taskColumns'
 
 usePageStore().pageData.title = 'Tasks'
 
-const tasks = ref<Tables<'tasks'>[] | null>(null)
-
+const tasks = ref<TasksWithProjects | null>(null)
 const getTasks = async () => {
-  const { data, error } = await supabase.from('tasks').select(`
-    *,
-    projects (
-      id,
-      name,
-      slug
-    )
-  `)
+  const { data, error } = await tasksWithProjectsQuery
   if (error) console.error(error)
 
   tasks.value = data
 }
 
 await getTasks()
-
-const columns: ColumnDef<Tables<'tasks'>>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/tasks/${row.original.id}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('name')
-      )
-    }
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left' }, row.getValue('status'))
-    }
-  },
-  {
-    accessorKey: 'due_date',
-    header: () => h('div', { class: 'text-left' }, 'Due Date'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left' }, row.getValue('due_date'))
-    }
-  },
-  {
-    accessorKey: 'projects',
-    header: () => h('div', { class: 'text-left' }, 'Project'),
-    cell: ({ row }) => {
-      return h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.projects.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('projects').name
-      )
-    }
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) => {
-      return h('div', { class: 'text-left' }, JSON.stringify(row.getValue('collaborators')))
-    }
-  }
-]
 </script>
