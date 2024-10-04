be260822ab6e5d1fcd99253d7a8ba03daafb0799
alessandro-giuzio <giuzio@icloud.com>
2024-10-04 17:22:09 +0200

    new todo app

---
+++ todo-app/src/views/TodoView.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>To Do list</h1>
+  <main>
+    <TodoList />
+  </main>
+</template>
+
+<script setup lang="ts">
+import TodoList from '../components/todo/TodoList.vue'
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 2rem;
+  color: beige;
+}
+</style>
