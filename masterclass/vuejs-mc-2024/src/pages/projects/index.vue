7e9eb2568849ff34fa7f641b521172c23112300b
alessandro-giuzio <giuzio@icloud.com>
2024-11-13 12:54:45 +0100

    Fetch collaborators across all projects

--- masterclass/vuejs-mc-2024/src/pages/projects/index.vue
+++ masterclass/vuejs-mc-2024/src/pages/projects/index.vue
@@ -1,20 +1,23 @@
-<template>
-  <DataTable v-if="projects" :columns="columns" :data="projects" />
-</template>
-
 <script setup lang="ts">
 import { columns } from '@/utils/tableColumns/projectsColumns'
 
 usePageStore().pageData.title = 'Projects'
 
-const projectLoader = useProjectsStore()
-const { projects } = storeToRefs(projectLoader)
-const { getProjects } = projectLoader
+const projectsLoader = useProjectsStore()
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
 
 await getProjects()
 
-const { getProfilesById } = useCollabs()
-const test = await getProfilesById(projects.value[0].collaborators)
-console.log('TEST ::', test)
-/* getGroupedCollabs(projects.value) */
+const { getGroupedCollabs, groupedCollabs } = useCollabs()
+
+await getGroupedCollabs(projects.value)
+
+console.log('TEST :: ', groupedCollabs.value)
+
+// getGroupedCollabs(projects.value)
 </script>
+
+<template>
+  <DataTable v-if="projects" :columns="columns" :data="projects" />
+</template>
