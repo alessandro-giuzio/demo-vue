07bcffce2f96fa3b8c9d8b2549b235c096ae423f
alessandro-giuzio <giuzio@icloud.com>
2024-12-15 16:05:01 +0100

    new route

--- todo-app/src/router/index.ts
+++ todo-app/src/router/index.ts
@@ -5,6 +5,7 @@ import TodoDetail from '../components/todo/TodoDetail.vue'
 import SharedNotesView from '@/views/SharedNotesView.vue'
 import ProjectsView from '@/views/ProjectsView.vue'
 import ProjectTasksView from '@/views/ProjectTasksView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
     },
     {
       path: '/about',
---
+++ todo-app/src/views/LoginView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login">login</div>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae, maxime.</p>
+</template>
+
+<script setup lang="ts"></script>
+<style>
+.login {
+  color: red;
+  border: 1px red solid;
+}
+</style>
