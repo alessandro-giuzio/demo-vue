5f8a1a18a792dac5c2a881c97e18ea99cc73857d
alessandro-giuzio <giuzio@icloud.com>
2024-12-27 12:23:53 +0100

    just before supabase

---
+++ projects-app/src/pages/projects/[id].vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Single Project {{ params.id }}</h1>
+  <RouterLink to="/projects">Projects</RouterLink>
+  <RouterLink to="/">Home</RouterLink>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const params = route.params as { id: string }
+</script>
---
+++ projects-app/src/pages/projects/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>Projects Page</h1>
+  <RouterLink to="/">Home</RouterLink>
+</template>
+
+<script setup lang="ts"></script>
