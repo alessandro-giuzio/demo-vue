095a3b6564264914b9124dcdbacc7862291989dd
alessandro-giuzio <giuzio@icloud.com>
2024-12-29 12:45:20 +0100

    tables

--- projects-app/src/pages/tasks/index.vue
+++ projects-app/src/pages/tasks/index.vue
@@ -1,18 +1,12 @@
 <template>
-  <main>
-    <h1>Tasks Page</h1>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        {{ task.name }}
-      </li>
-    </ul>
-    <RouterLink to="/">Home</RouterLink>
-  </main>
+  <DataTable :columns="columns" :data="payments" />
 </template>
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
-import { ref } from 'vue'
+import { h, ref } from 'vue'
 import type { Tables } from '../../../database/types'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
 
 const tasks = ref<Tables<'tasks'>[] | null>(null)
 ;(async () => {
@@ -21,4 +15,42 @@ const tasks = ref<Tables<'tasks'>[] | null>(null)
   tasks.value = data
   console.log('Tasks:', tasks.value)
 })()
+
+type Payment = {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com'
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com'
+  }
+  // ...
+]
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    }
+  }
+]
 </script>
