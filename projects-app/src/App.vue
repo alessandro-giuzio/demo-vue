b1eaefeb8bde8c512019acab1c24fb0e8c4fbee6
alessandro-giuzio <giuzio@icloud.com>
2025-01-08 12:10:52 +0100

    lesson 78

--- projects-app/src/App.vue
+++ projects-app/src/App.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
-const { activeError } = storeToRefs(useErrorStore())
+import { supabase } from './lib/supabaseClient'
 
+const { activeError } = storeToRefs(useErrorStore())
+const authStore = useAuthStore()
 const errorStore = useErrorStore()
 
 onErrorCaptured((error) => {
   errorStore.setError({ error })
+})
+
+onMounted(async () => {
+  const { data } = await supabase.auth.getSession()
+  if (data.session?.user) await authStore.setAuth(data.session)
 })
 </script>
 
--- projects-app/src/stores/auth.ts
+++ projects-app/src/stores/auth.ts
@@ -1,44 +1,58 @@
-
-import type { Tables} from "database/types"
-import type {Session, User} from "@supabase/supabase-js"
+// Import necessary types and functions
+import type { Tables } from "database/types"
+import type { Session, User } from "@supabase/supabase-js"
 import { userRegQuery } from "@/utils/supaQueries"
+import { supabase } from "@/lib/supabaseClient"
 
 
-
+// Define the authentication store
 export const useAuthStore = defineStore('auth-store', () => {
+  // State to hold the current user
   const user = ref<null | User>(null)
+  // State to hold the user registration details
   const userReg = ref<null | Tables<'users'>>(null)
 
+  // Function to set user registration details
   const setUserReg = async () => {
-    if(!user.value){
+    // If there is no user, set userReg to null and return
+    if (!user.value) {
       userReg.value = null
       return
     }
 
-    if(!userReg.value || userReg.value.id !== user.value.id){
-      const {data} = await userRegQuery(user.value.id)
-
+    // If userReg is not set or the user ID has changed, fetch the user registration details
+    if (!userReg.value || userReg.value.id !== user.value.id) {
+      const { data } = await userRegQuery(user.value.id)
       userReg.value = data || null
-
     }
   }
 
+  // Function to set the authentication state
   const setAuth = async (userSession: null | Session = null) => {
-    if(!userSession) {
+    // If there is no user session, set user to null and return
+    if (!userSession) {
       user.value = null
       return
     }
+    // Set the user state and fetch user registration details
     user.value = userSession.user
     await setUserReg()
   }
-
+  // Function to get the current session
+  const getSession = async () => {
+    const { data } = await supabase.auth.getSession()
+    if (data.session?.user) await setAuth(data.session)
+  }
+  // Return the state and functions to be used in the store
   return {
     user,
     userReg,
-    setAuth
-
+    setAuth,
+    getSession
   }
 })
+
+// Enable Hot Module Replacement (HMR) for the store
 if (import.meta.hot) {
   import.meta.hot.accept(acceptHMRUpdate(useAuthStore, import.meta.hot))
 }
