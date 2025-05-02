4bcab2c0283bccacad6a6f461f3f0933801b1fac
Martin Staudt <ms@aptex.de>
2025-05-02 11:31:12 +0200

    add routing

--- projects-app/src/router/index.ts
+++ projects-app/src/router/index.ts
@@ -1,5 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import {routes} from 'vue-router/auto-routes'
+
+import TasksComponent from '@/pages/tasks/index.vue'
+import SingleTaskComponent from '@/pages/tasks/[id].vue'
+import ProjectComponent from '@/pages/projects/index.vue'
+import SingleProjectComponent from '@/pages/projects/[slug].vue'
+
+const routes = [
+  { path: '/', redirect: '/task' },
+  { path: '/task', component: TasksComponent },
+  { path: '/task/:taskId', component: SingleTaskComponent },
+  { path: '/project', component: ProjectComponent },
+  { path: '/project/:slug', component: SingleProjectComponent },
+]
+
+/*
+  PSEUDO CODE, DOESNT WORK
+  const newTask = await createTask({...someData});
+  redirect(`/task/${newTask.id}`);
+
+  const newProject = await createProject({...someData});
+  redirect(`/project/${newProject.slug}`);
+
+*/
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
